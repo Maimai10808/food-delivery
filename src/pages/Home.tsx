55fd25da220ba02a41dffc9753ba6e31a443497d
Maimai10808 <331686549@qq.com>
2026-02-01 00:18:59 +0800

    Categories Card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,27 @@
+import Categories from "../Category";
 import { Nav } from "../components/Nav";
 
 export function Home() {
   return (
     <div className="w-full bg-slate-400 min-h-screen">
       <Nav />
+
+      <div className="flex flex-wrap justify-center items-center gap-10 ">
+        {Categories.map((item) => {
+          return (
+            <div
+              className="w-35 h-40 bg-white flex-col items-start p-5 gap-10
+                         justify-start text-xl font-semibold text-gray-600
+                         rounded-md shadow-2xl hover:bg-green-200 cursor-pointer
+                         transition-all duration-200
+                         "
+            >
+              {item.icon}
+              {item.name}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
